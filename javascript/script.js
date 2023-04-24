d8e91e607497b0d79776f85c495470526559ef2a
camh00 <113644669+camh00@users.noreply.github.com>
2023-04-24 00:51:38 -0600

    Did some work

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
     $("#stt-wrapper").click(function() {
-      console.log('I AM CLICKED');
       $("html, body").animate({ scrollTop: 0 }, 0);
       return false;
     });

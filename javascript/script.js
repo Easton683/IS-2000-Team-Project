fba5497461a8f45345c2956afc26631afc3b9c06
camh00 <113644669+camh00@users.noreply.github.com>
2023-04-25 22:00:40 -0600

    adding comments

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+// functions for buy page
 $(document).ready(function() {
     $("#stt-wrapper").click(function() {
       $("html, body").animate({ scrollTop: 0 }, 0);
@@ -9,6 +10,7 @@ $(document).ready(function() {
     alert("Thank you for your purchase!");
   }
   
+// function for sell page
 function ssubmit() {
 	alert("Thank you! Our team will reach out to you with their appraisal within the next 24 hours!");
 }
